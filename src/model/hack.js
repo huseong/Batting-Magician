d9fe4ed40cf387f7b2f9ff119c96450e5072bc7a
huseong <huseong734@gmail.com>
2017-12-02 09:05:07 +0900

    Date Module (date-utils)

--- src/model/hack.js
+++ src/model/hack.js
@@ -1,17 +1,20 @@
 const mongoose  = require('mongoose')
+require('date-utils')
 
 const schema = new mongoose.Schema({
-  date : { type : Date, default : Date.now() },
+  date : Date,
   hackName : String,
   userName : String,
   id : String
 })
 
 schema.statics.create = (hackName, userName, id) => {
+  const date = new Date()
   let hack = new hack({
     hackName : hackName,
     userName : userName,
-    id : id
+    id : id,
+    date : date.toFormat('YYYY-MM-DD HH24:MI:SS')
   })
   hack.save(err => { console.log('DB Error : ', err) })
 }
--- src/model/user.js
+++ src/model/user.js
@@ -1,29 +1,36 @@
 const mongoose = require('mongoose')
+require('date-utils')
 const schema = new mongoose.Schema({
     userInfo : {
         id : String,
         name : String,
-        money : Number
+        money : Number,
+        isBanned : Boolean,
+        lastEnter : Date
     },
     currentStatus : {
-        room: Number
+        server: String
     }
 })
 
 schema.statics.create = (id, name) => {
+    const date = new Date()
     const newUser = new user({
         userInfo : {
             id : id,
             name : name,
-            money : 0
+            money : 0,
+            isBanned : false,
+            lastEnter : date.toFormat('YYYY-MM-DD HH24:MI:SS')
         },
         currentStatus : {
-            room : 0
+            server : 'Lobby'
         }
     })
     newUser.save(err => {
         console.log('User Save Error : ' + err)
     })
+    return newUser
 }
 
 const user = mongoose.model('user', schema)
