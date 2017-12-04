254984e3ef6d771176a4a7dea5d3754f945343d0
huseong <huseong2@naver.com>
2017-12-04 14:32:46 +0900

    Port Separation

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const frontServer = require(serverUrl + 'frontServer.js')
 const lobbyServer = require(serverUrl + 'lobbyServer.js')
 const gameServer = require(serverUrl + 'gameServer.js')
 
-frontServer(new Server(process.env.FRONTPORT))
-lobbyServer(new Server(process.env.LOBBYPORT))
-gameServer(new Server(process.env.GAMEPORT))
+frontServer(new Server(process.env.KOFRONTPORT))
+lobbyServer(new Server(process.env.KOLOBBYPORT))
+gameServer(new Server(process.env.KOGAMEPORT))
 
