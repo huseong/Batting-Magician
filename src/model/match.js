ef8548057ddbab67d1082da97be2c9b19a404f5c
huseong <huseong2@naver.com>
2017-12-04 14:33:12 +0900

    Add Create Function

--- src/model/match.js
+++ src/model/match.js
@@ -2,22 +2,42 @@
 */
 
 const mongoose = require('mongoose')
-
+const Error = require('./error.js')
+require('date-utils')
 const schema = new mongoose.Schema({
   meta : {
     id : Number, // 해당 방에 대한 고유 식별자이다.
+    serverName : String,
     date : Date,
     name : String,
     type : String
   },
   info : {
-    user : Array,
+    users : [String],
     status : String,
-    horses : Array
+    horses : [Number]
   }
 })
 
 let currentRoomID = 1 // 방 번호이다. 매번 하나씩 늘어난다.
 
+schema.statics.create = (server, type, users) => {
+  let newMatch = new room({
+    meta : {
+      id : server.getRoomID,
+      serverName : server.info.name,
+      date : (new Date()).toFormat('YYYY-MM-DD HH24:MI:SS'),
+      name : '후성배 경마 경주',
+      type : type
+    },
+    info : {
+      users : users.map(user => user.id),
+      status : 'Wait',
+      
+    }
+  })
+}
 
 const room = mongoose.model('room', room)
+
+module.exports = room
