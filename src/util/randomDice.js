1e89920cee1489fe2da069d02ca9de67ea6fb852
huseong <huseong2@naver.com>
2017-12-12 12:21:37 +0900

    Random IDce

---
+++ src/util/randomDice.js
@@ -0,0 +1,10 @@
+// TODO: 주사위 굴린 값을 반환한다.
+
+module.exports = (weights, sum) => {
+  let randomValue = Math.floor(Math.random() * sum) + 1
+  for(let i = 0; i<weights.length; i++) {
+    randomValue -= weights[i]
+    if(randomValue <= 0)
+      return i + 1
+  }
+}
