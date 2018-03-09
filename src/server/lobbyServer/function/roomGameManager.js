8eab554187619491b997817f60535be91aeaaa9b
huseong <huseong734@gmail.com>
2018-03-09 22:11:20 +0900

    Check Hack

--- src/server/lobbyServer/function/roomGameManager.js
+++ src/server/lobbyServer/function/roomGameManager.js
@@ -17,15 +17,20 @@ module.exports = (socket, roomServer) =>
       User.checkStatus(socket, 'Lobby') // 플레이어가 로비에 
       .then(user => new Promise(async function (resolve, reject) {
         console.log(param)
-        if(!(param.name.length < 13)) // 방의 이름의 제한에 맞지 않는 요청이 들어온 경우
+        if(!(param.name.length < 13)) { // 방의 이름의 제한에 맞지 않는 요청이 들어온 경우
+          console.log('name')
           Hack.create('Name Over in Create Room')
           return reject('Client Hack')
+        }
         if(!(1 < param.userPersonnel && param.userPersonnel < 31)) { // 유저 인원수 제한 값에 맞지 않는 인원이 들어온 경우
+          console.log('user over')
           Hack.create('User Personnel Over in Create Room')
           return reject('Client Hack')
         }
         if(!(1 < param.runnerPersonnel && param.runnerPersonnel < 7)) { // 경주마의 제한값에 맞지 않는
+          console.log('runner over')
           Hack.create('Runner Personnel Over in Create Room')
+          return reject('Client Hack')
         }
         socket.emit('room making start')
         const match = await RoomMatch.create(socket.server, param) // DB상에서 새로운 매치를 만들어낸다.
